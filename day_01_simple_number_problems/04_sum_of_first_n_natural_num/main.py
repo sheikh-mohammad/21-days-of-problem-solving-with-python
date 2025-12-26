18692d8e2afbab02868c05d98ff019af87fe8e93
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-26 16:47:09 +0500

    feat: Add program to calculate sum of first N natural numbers.

--- day_01_simple_number_problems/04_sum_of_first_n_natural_num/main.py
+++ day_01_simple_number_problems/04_sum_of_first_n_natural_num/main.py
@@ -1,5 +1,10 @@
 def main() -> None:
-    pass
+    n : int = int(input("Enter a number: "))
+    sum : int = 0
+    for num in range(1, n + 1):
+        sum += num
+        
+    print(sum)
 
 if __name__ == '__main__':
     main()

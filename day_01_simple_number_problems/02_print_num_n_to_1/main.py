15401a89299baf906ac3e6e10764a711ca0a6aa7
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-26 16:49:02 +0500

    feat: add script to print numbers from N down to 1

--- day_01_simple_number_problems/02_print_num_n_to_1/main.py
+++ day_01_simple_number_problems/02_print_num_n_to_1/main.py
@@ -1,13 +1,13 @@
 def main() -> None:
     # Get integer input from the user for the starting number
-    end_num : int = int(input("Enter a number: "))
+    start_num : int = int(input("Enter a number: "))
     
-    # Iterate from 'end_num' down to 1
+    # Iterate from 'start_num' down to 1
     # range(start, stop, step)
-    # start = end_num
+    # start = start_num
     # stop = 0 (calculated as 1 - 1, so the loop includes 1 but excludes 0)
     # step = -1 (decrements the counter)
-    for num in range(end_num, 1 - 1, -1):
+    for num in range(start_num, 1 - 1, -1):
         print(num, end=" ")
 
 if __name__ == '__main__':

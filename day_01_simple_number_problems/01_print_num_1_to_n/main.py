6a55d2956104180121b60dff377fe885a7e93c34
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-26 16:20:57 +0500

    feat: add script to print numbers from 1 to N.

--- day_01_simple_number_problems/01_print_num_1_to_n/main.py
+++ day_01_simple_number_problems/01_print_num_1_to_n/main.py
@@ -5,13 +5,13 @@
 
 def main() -> None:
     # Get integer input from the user to define the upper limit
-    num : int = int(input("Enter a number: "))
+    end_num : int = int(input("Enter a number: "))
     
     # Iterate from 1 up to and including 'num'
     # range(start, stop) includes start but excludes stop, so we use num + 1
-    for i in range(1, num + 1):
+    for num in range(1, end_num + 1):
         # Print the current number with a space separator instead of a newline
-        print(i, end=" ")
+        print(num, end=" ")
 
 if __name__ == '__main__':
     main()

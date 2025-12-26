32ac6330ab27d25bbecca4ac61342b374584f145
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-26 16:16:10 +0500

    Changed the output format

--- day_01_simple_number_problems/01_print_num_1_to_n/main.py
+++ day_01_simple_number_problems/01_print_num_1_to_n/main.py
@@ -2,7 +2,7 @@ def main() -> None:
     num : int = int(input("Enter a number: "))
     
     for i in range(1, num + 1):
-        print(i)    
+        print(i, end=" ")
 
 if __name__ == '__main__':
     main()

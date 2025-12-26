222828d8073ea3249d08f36b56a82301dfe429b1
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-26 16:40:46 +0500

    feat: add script to print even numbers from 1 to a given limit

--- day_01_simple_number_problems/03_print_even_num_1_to_n/main.py
+++ day_01_simple_number_problems/03_print_even_num_1_to_n/main.py
@@ -1,5 +1,9 @@
 def main() -> None:
+    end_num : int = int(input("Enter a ending number: "))
     
+    for num in range(1, end_num + 1):
+        if num % 2 == 0:
+            print(num, end=" ")
 
 if __name__ == '__main__':
     main()

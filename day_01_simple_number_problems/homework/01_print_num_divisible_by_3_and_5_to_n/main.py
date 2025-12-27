539321448c48511e11de459d8299d5e5b727c9ce
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-27 18:48:58 +0500

    feat: add script to print numbers divisible by 3 and 5 up to n

--- day_01_simple_number_problems/homework/01_print_num_divisible_by_3_and_5_to_n/main.py
+++ day_01_simple_number_problems/homework/01_print_num_divisible_by_3_and_5_to_n/main.py
@@ -1,5 +1,9 @@
 def main() -> None:
+    n : int = int(input("Enter a number: "))
     
+    for num in range(1, n + 1):
+        if (num % 3 == 0) and (num % 5 == 0):
+            print(num, end=" ")
 
 if __name__ == '__main__':
     main()

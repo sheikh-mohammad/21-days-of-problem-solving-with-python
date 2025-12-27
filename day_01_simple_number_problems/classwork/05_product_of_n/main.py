4056a1441495a585cd4fecd112e089f422f03a3e
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-27 17:52:49 +0500

    feat: add program to calculate the product of n numbers.

--- day_01_simple_number_problems/classwork/05_product_of_n/main.py
+++ day_01_simple_number_problems/classwork/05_product_of_n/main.py
@@ -1,5 +1,11 @@
 def main() -> None:
+    n : int = int(input("Enter a number: "))
+    factorial : int = 1
     
+    for i in range(n, 1 - 1, -1):
+            factorial *= i
 
+    print(factorial)
+    
 if __name__ == '__main__':
     main()

63a50d15a6677a455da47a890268271c4e6c4963
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-12-27 18:14:36 +0500

    feat: add program to calculate the sum of all even numbers up to a given input `n`

--- day_01_simple_number_problems/classwork/06_sum_of_all_even_num_to_n/main.py
+++ day_01_simple_number_problems/classwork/06_sum_of_all_even_num_to_n/main.py
@@ -1,5 +1,16 @@
 def main() -> None:
+    n : int = int(input("Enter a number: "))
+    sum_of_even : int = 0
+    evens : list[str | int] = []
     
+    for num in range(2, n + 1):
+        if num % 2 == 0:
+            sum_of_even += num
+            evens.append(num)
+            
+    state : str = " + ".join([str(even) for even in evens])            
+            
+    print(sum_of_even, f"({state})")
 
 if __name__ == '__main__':
     main()

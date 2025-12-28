8c4d3fa0c90855f7a421152cddfde22dd4528aef
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-28 10:53:14 +0500

    feat: Add solution to print sum of all odd number up to n

--- day_01_simple_number_problems/homework/02_sum_of_odd_num_to_n/main.py
+++ day_01_simple_number_problems/homework/02_sum_of_odd_num_to_n/main.py
@@ -1,7 +1,18 @@
 def main() -> None:
     n : int = int(input("Enter a number: "))
     
+    sum_of_odd : int = 0
     
+    odds : list[int | str] = []
+    
+    for num in range(1, n + 1):
+        if num % 2 != 0:
+            sum_of_odd += num
+            odds.append(num)
+            
+    state = " + ".join([str(odd) for odd in odds])
+    
+    print(sum_of_odd, f"( {state} )")
 
 if __name__ == '__main__':
     main()

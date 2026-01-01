c636bcd5a278e3a2b71ca6d694bed6c559b06582
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-01 18:25:35 +0500

    edit: rename variable start_num to n in numbers n to 1 exercise

--- day_01_simple_number_problems/classwork/02_num_n_to_1/main.py
+++ day_01_simple_number_problems/classwork/02_num_n_to_1/main.py
@@ -1,13 +1,13 @@
 def main() -> None:
     # Get integer input from the user for the starting number
-    start_num : int = int(input("Enter a number: "))
+    n : int = int(input("Enter a number: "))
     
-    # Iterate from 'start_num' down to 1
+    # Iterate from 'n' down to 1
     # range(start, stop, step)
-    # start = start_num
+    # start = n
     # stop = 0 (calculated as 1 - 1, so the loop includes 1 but excludes 0)
     # step = -1 (decrements the counter)
-    for num in range(start_num, 1 - 1, -1):
+    for num in range(n, 1 - 1, -1):
         print(num, end=" ")
 
 if __name__ == '__main__':

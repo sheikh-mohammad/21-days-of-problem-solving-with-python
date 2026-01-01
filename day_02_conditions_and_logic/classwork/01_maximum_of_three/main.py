f551efc85819c3658f4871d3be97e8bf129f32be
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-01 18:15:01 +0500

    edit: implement maximum of three numbers logic with error handling

--- day_02_conditions_and_logic/classwork/01_maximum_of_three/main.py
+++ day_02_conditions_and_logic/classwork/01_maximum_of_three/main.py
@@ -1,5 +1,18 @@
 def main() -> None:
-    
+    try:
+        number_1 : int = int(input("Enter First number: "))
+        number_2 : int = int(input("Enter Second Number: "))
+        number_3 : int = int(input("Enter Third Number: "))
+        if number_1 >= number_2 and number_1 >= number_3:
+            print(number_1)
+        elif number_2 >= number_1 and number_2 >= number_3:
+            print(number_2)
+        else:
+            print(number_3)
+    except ValueError:
+        print("Invalid Input!")
+    except Exception as e:
+        print(f"Error occured: {e}")
 
 if __name__ == '__main__':
     main()

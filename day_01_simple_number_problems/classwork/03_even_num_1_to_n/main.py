2e7b2845911536fd8a034931c6dcfb2f8e590c24
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-01 18:23:20 +0500

    edit: rename variable end_num to n in even numbers exercise

--- day_01_simple_number_problems/classwork/03_even_num_1_to_n/main.py
+++ day_01_simple_number_problems/classwork/03_even_num_1_to_n/main.py
@@ -1,7 +1,7 @@
 def main() -> None:
-    end_num : int = int(input("Enter a ending number: "))
+    n : int = int(input("Enter a number: "))
     
-    for num in range(1, end_num + 1):
+    for num in range(1, n + 1):
         if num % 2 == 0:
             print(num, end=" ")
 

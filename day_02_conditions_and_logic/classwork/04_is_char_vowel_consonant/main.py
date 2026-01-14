374493cafa13998b590330a1e6983938b1043e39
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-01-15 00:44:47 +0500

    feat: implement vowel consonant checker program in day 02 classwork

--- day_02_conditions_and_logic/classwork/04_is_char_vowel_consonant/main.py
+++ day_02_conditions_and_logic/classwork/04_is_char_vowel_consonant/main.py
@@ -1,5 +1,16 @@
-def main():
-  pass
+def main() -> None:
+  letter : str = input("Enter a letter: ")
+
+  if len(letter) > 1:
+    print("Please enter a single letter!")
+  else:
+    if letter.isalpha() == True:
+      if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+        print("Vowel")
+      else:
+        print("Constant")
+    else:
+      print("Please enter a alphabet letter")
 
 if __name__ == '__main__':
   main()
